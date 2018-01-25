d3cf212b3cb706fbad5acd59a57e5e1f77c23608
Nick Volkert <nick@nickvolkert.com>
2018-01-24 20:51:25 -0600

    added google chart

---
+++ js/chart.js
@@ -0,0 +1,33 @@
+//Trying out google charts *Nick V*
+
+// Load the Visualization API and the piechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+        var fordata = 3;
+        var againstdata = 1;
+      // Create the data table.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'What people are saying');
+      data.addColumn('number', 'number for');
+      data.addRows([
+        ['for', fordata],
+        ['against', againstdata]
+      ]);
+
+      // Set chart options
+      var options = {'title':'People for this subject',
+                     'width':600,
+                     'height':600};
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('googleChart'));
+      chart.draw(data, options);
+    }
+    drawChart();
