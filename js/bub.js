f01057939eeeca91e3cad039a7ec206b252c447d
Nick Volkert <nick@nickvolkert.com>
2018-01-27 13:02:38 -0600

    added search term to header adjusted curtain opacity rounded corners

--- js/bub.js
+++ js/bub.js
@@ -30,7 +30,7 @@ database.ref().on("value", function(snapshot) {
 
 // --------------------------------------------------------------
 
-//this doesn't work but would be the modal when a search is performed - Nick
+//fires an overlay and animation when a user searches a term in twitter - Nick
 function myLocationCurtain(){
           locationCurtainDiv = $('<div id="intCurtain"><div class="loader"></div></div>');
           console.log("modal fire is working");
@@ -44,6 +44,10 @@ function myLocationCurtain(){
 $("#submitTweetSubject").on("click", function(event) {
   // Prevent default behavior
   event.preventDefault();
+  //Comment or Uncomment this for the loading overlay - Nick
   myLocationCurtain();
-
+  //pushes search term to header of block below input - Nick
+  var userSearch = $("#tweetSubject").val().trim();
+  $("#tweetSubjectHeader").text(userSearch);
+  $("#carouselId").hide();
 });
